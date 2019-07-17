27082cc77326eafeb0a51c44fb8434d4819e5cc0
kurrasowmya <sowmyakurra98@gmail.com>
2019-07-17 11:00:37 +0530

    annotations task5 added

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,5 +1,6 @@
 package com.stackroute;
 
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import com.stackroute.domain.Movie;
 import com.stackroute.domain.MovieConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -7,8 +8,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(MovieConfiguration.class);
-        Movie movie = annotationConfigApplicationContext.getBean("movie", Movie.class);
-        movie.display();
+        BeanLifecycleDemoBean beanlifecycle = annotationConfigApplicationContext.getBean("BeanLifecycleDemoBean", BeanLifecycleDemoBean.class);
+        beanlifecycle.customInit();
+        beanlifecycle.customDestroy();
 
 
     }
--- src/main/java/com/stackroute/domain/MovieConfiguration.java
+++ src/main/java/com/stackroute/domain/MovieConfiguration.java
@@ -1,11 +1,19 @@
 package com.stackroute.domain;
 
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(basePackages="com.stackroute.domain")
 @PropertySource("classpath:actorinfo.properties")
 public class MovieConfiguration {
+    @Bean(name="BeanLifecycleDemoBean",initMethod = "customInit",destroyMethod = "customDestroy")
+    public BeanLifecycleDemoBean getBeanLifecycledemoBean()
+    {
+        return new BeanLifecycleDemoBean();
+    }
+
+
 
 
 }
